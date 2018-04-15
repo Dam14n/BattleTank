4dd8c9cb3983aeaa783849ca586a24ca4b6ed255
Dam14n <damian.jjorda@gmail.com>
2018-04-15 19:46:36 -0300

    Migrated to 4.19

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "GameFramework/Actor.h"
 #include "TankAIController.h"
+#include "GameFramework/Actor.h"
 
 
 void ATankAIController::Tick(float DeltaTime)
